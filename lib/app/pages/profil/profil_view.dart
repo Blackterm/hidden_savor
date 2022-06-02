a46f09db8bddcef1ae6742d6580e27e210f8e83d
BlackTerm <muratcan.eravsar@hotmail.com>
2022-06-02 22:13:20 +0300

    added somethings

---
+++ lib/app/pages/profil/profil_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilView extends StatefulWidget {
+  const ProfilView({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilView> createState() => _ProfilViewState();
+}
+
+class _ProfilViewState extends State<ProfilView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
